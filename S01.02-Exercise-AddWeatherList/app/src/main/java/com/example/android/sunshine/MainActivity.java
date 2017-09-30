43026e4a6941061a456f6e8a7907898c1493ba08
Steve Gremban <steve.gremban@gmail.com>
2017-09-30 00:30:34 -0500

    Exercise: Add Scrolling Weather

--- S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
+++ S01.02-Exercise-AddWeatherList/app/src/main/java/com/example/android/sunshine/MainActivity.java
@@ -17,10 +17,12 @@ package com.example.android.sunshine;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO (1) Create a field to store the weather display TextView
+    private TextView tv_weather_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,9 +30,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_forecast);
 
         // TODO (2) Use findViewById to get a reference to the weather display TextView
+        tv_weather_data = (TextView) findViewById(R.id.tv_weather_data);
 
         // TODO (3) Create an array of Strings that contain fake weather data
+        String[] weatherStringArray = {"Forcast for Sunday 9/31/17\n\n", "Sunny\n\n", "Humidity: 30%\n\n",
+                "Chance of Precipitation: 20%\n\n", "High: 90 degrees\n\n", "Low: 75 degrees\n\n",
+                "Forcast for Monday 10/1/17\n\n", "Overcast\n\n", "Humidity: 40%\n\n",
+                "Chance of Precipitation: 30%\n\n", "High: 87 degrees\n\n", "Low: 72 degrees\n\n",
+                "Forcast for Monday 10/2/17\n\n", "Thunder Storms\n\n", "Humidity: 60%\n\n",
+                "Chance of Precipitation: 60%\n\n", "High: 82 degrees\n\n", "Low: 68 degrees\n\n"};
 
         // TODO (4) Append each String from the fake weather data array to the TextView
+        for (String weatherString: weatherStringArray){
+            tv_weather_data.append(weatherString);
+        }
     }
 }
